074c180f64a15d30fb58123342799581598855ca
Thierry Bela <bnanga@gmail.com>
2018-02-18 23:35:58 -0500

    Enable server timing api

---
+++ README.md
@@ -0,0 +1,6 @@
+HTML(5) Server Timing API
+------------------------------
+
+This plugin enable Server Timing HTTP headers. See https://w3c.github.io/server-timing/
+
+This plugin must run as the last plugin in system group. You need to reorder plugins and put it as the last.
---
+++ servertiming.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @package     GZip.HTML
+ * @subpackage  Server.Timing
+ *
+ * @copyright   Copyright (C) 2013 Thierry Bela
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+class PlgSystemServerTiming extends JPlugin {
+
+    public function onAfterRender() {
+
+        if (!$this->params->get('servertiming')) {
+
+            return;
+        }
+
+		$data = $this->getTimingData();
+
+		$header = [];
+
+		foreach ($data['marks'] as $k => $mark) {
+
+			$header[] = substr('00'.($k + 1), -3).'-'.preg_replace('#[^A-Za-z0-9]#', '', $mark->tip).';dur='.$mark->time; //.';memory='.$mark->memory;
+		}
+
+		$header[] = 'total;dur='.$data['totalTime']; //.';memory='.$data['totalMemory'];
+		header('Server-Timing: '.implode(',', $header));        
+    }
+	
+	/**
+	 * Display profile information.
+	 *
+	 * @return  string
+	 *
+	 * @since   2.5
+	 */
+	protected function getTimingData()
+	{
+		$totalTime = 0;
+		$totalMem  = 0;
+		$marks     = array();
+
+		foreach (JProfiler::getInstance('Application')->getMarks() as $mark)
+		{
+			$totalTime += $mark->time;
+			$totalMem  += (float) $mark->memory;
+
+			$marks[] = (object) array(
+				'time'   => $mark->time,
+				'memory' => $mark->memory,
+				'tip'    => $mark->label,
+			);
+		}
+
+        return ['totalTime' => $totalTime, 'totalMemory' => $totalMem, 'marks' => $marks];
+    }
+
+}
